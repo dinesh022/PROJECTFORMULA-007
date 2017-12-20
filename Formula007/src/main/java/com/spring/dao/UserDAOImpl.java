e9b6079bc59f0d74e55f1cafc668d2bd58a49131
dinesh <dinesh@Dinesh>
2017-12-20 21:20:18 +0530

    FIRST COMMIT

---
+++ Formula007/src/main/java/com/spring/dao/UserDAOImpl.java
@@ -0,0 +1,43 @@
+package com.spring.dao;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.spring.model.Users;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+	public UserDAOImpl()
+	{}
+
+
+@Autowired
+private SessionFactory sessionFactory;
+
+
+
+public UserDAOImpl(SessionFactory sessionFactory) {
+	this.sessionFactory=sessionFactory;
+}
+
+
+public boolean saveUser(Users users) {
+	
+	
+	try {
+		Session	session =sessionFactory.openSession();
+		Transaction tx=session.beginTransaction();
+		session.saveOrUpdate(users);
+		tx.commit();
+		return true;
+	} catch (HibernateException e) {
+		
+		e.printStackTrace();
+	}
+	return false;
+}
+
+	
+}
